3096e0b00b0e5959879be7517d6dda136e7b9b96
Sujanth <sujanth1987@gmail.com>
2017-07-19 21:03:09 +1000

    Outlets and actions created for UI

--- Seven Swifty Words/ViewController.swift
+++ Seven Swifty Words/ViewController.swift
@@ -10,16 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cluesLabel: UILabel!
+    @IBOutlet weak var answersLabel: UILabel!
+    @IBOutlet weak var currentAnswer: UITextField!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func submitTapped(_ sender: UIButton) {
     }
-
+    
+    @IBAction func clearTapped(_ sender: UIButton) {
+    }
+    
 
 }
 
